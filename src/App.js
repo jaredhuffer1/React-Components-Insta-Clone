a80e238f39ffab1bd77399cb9b0e67f28ca3d2ad
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-03-17 22:44:13 -0400

    importing

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@
 */
 
 // Import the state hook
-import React from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import React, { useState } from 'react';
+// Import the Posts (plural!) and SearchBar components, since they are used inside App component'
 // Import the dummyData 
+import './dummy-data';
 import './App.css';
 
 const App = () => {
